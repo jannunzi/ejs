11e24a1b9a94e36837ba55be603ae582d026038e
Jose Annunziato <jannunzi@gmail.com>
2015-09-03 13:23:32 -0400

    2.0) index.ejs - Hello from EJS

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
 app.listen(3000);
